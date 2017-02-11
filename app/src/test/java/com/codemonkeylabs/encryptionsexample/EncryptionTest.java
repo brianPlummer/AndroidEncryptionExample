05d8f098c548723a7d90203bf3945123dc5b8b7d
Brian Plummer <brian.r.plummer@gmail.com>
2017-02-10 22:15:26 -0800

    add in AES/CBC/PKCS7Padding and test

--- app/src/test/java/com/codemonkeylabs/encryptionsexample/EncryptionTest.java
+++ app/src/test/java/com/codemonkeylabs/encryptionsexample/EncryptionTest.java
@@ -1,34 +1,25 @@
 package com.codemonkeylabs.encryptionsexample;
 
-import android.test.InstrumentationTestCase;
-
 import com.codemonkeylabs.encryptionexample.app.AESEncryptDecrypt;
 import com.codemonkeylabs.encryptionexample.app.RSAEncryptDecrypt;
 import com.codemonkeylabs.encryptionexample.app.Util;
 
 import org.apache.commons.io.IOUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.UnsupportedEncodingException;
-import java.net.URL;
 import java.security.KeyPair;
 import java.util.Arrays;
 
-/**
- * Created by brianplummer on 9/2/14.
- */
+import static org.junit.Assert.assertTrue;
+
 public class EncryptionTest
 {
     private String testText = null;
@@ -78,6 +69,34 @@ public class EncryptionTest
         assertTrue(unencryptedString.startsWith("All this while Tashtego, Daggoo, and Queequeg"));
     }
 
+
+    @Test
+    public void testAESEncryptionCBCPadding7() throws UnsupportedEncodingException
+    {
+
+        ByteArrayInputStream plainTextInputStream = new ByteArrayInputStream(testText.getBytes("UTF-8"));
+        ByteArrayOutputStream encOutputStream = new ByteArrayOutputStream(1024 * 100);
+
+        byte[] iv = AESEncryptDecrypt.aesEncrypt(plainTextInputStream,
+                AESEncryptDecrypt.NOT_SECRET_ENCRYPTION_KEY.toCharArray(),
+                AESEncryptDecrypt.AESCipherType.AES_CBC_PKCS7Padding,
+                encOutputStream);
+
+
+        ByteArrayInputStream encInputStream = new ByteArrayInputStream(encOutputStream.toByteArray());
+        ByteArrayOutputStream plainTextOutputStream = new ByteArrayOutputStream(1024 * 100);
+
+        AESEncryptDecrypt.aesDecrypt(encInputStream,
+                AESEncryptDecrypt.NOT_SECRET_ENCRYPTION_KEY.toCharArray(),
+                iv,
+                AESEncryptDecrypt.AESCipherType.AES_CBC_PKCS7Padding,
+                plainTextOutputStream);
+
+
+        String unencryptedString = new String(plainTextOutputStream.toByteArray(),"UTF-8");
+
+        assertTrue(unencryptedString.startsWith("All this while Tashtego, Daggoo, and Queequeg"));
+    }
 
     @Test
     public void testAESEncryptionCTR() throws UnsupportedEncodingException
